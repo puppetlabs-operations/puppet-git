eefb80e6fef50427dc3af61f6f72110500948044
Adrien Thebo <adrien@puppetlabs.com>
2012-08-22 14:33:16 -0700

    Import code

---
+++ manifests/params.pp
@@ -0,0 +1,16 @@
+# = Class: git::params
+#
+class git::params {
+  case $operatingsystem {
+    'Debian': { $packages = 'git' }
+    'Ubuntu': { $packages = 'git-core' }
+    'Ubuntu': {
+      case $operatingsystemrelease {
+        'Hardy', 'Lucid': { $packages = 'git-core' }
+        default: { $packages = 'git' }
+      }
+    'SLES', 'Ubuntu', 'RedHat', 'Fedora': { $packages = 'git' }
+    'FreeBSD': { $packages = 'devel/git' }
+    default:   { fail("No git package known for operating system ${operatingsystem}") }
+  }
+}
